f32fd58ebb1d78704c8644338298851f1fc191fb
noahsausen <noahsausen@gmail.com>
2025-07-01 22:15:03 -0400

    React setup and v1 site

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import githubIcon from "./assets/github.svg"
+import emailIcon from "./assets/email.svg"
+import instagramIcon from "./assets/instagram.svg"
+
+document.body.style.background = 'hsl(0, 0%, 5%)';
+
+export default function App() {
+  return (
+    <div className="App">
+      <title>Noah Sausen</title>
+      <Heading/>
+      <Details/>
+      <LinkContainer/>
+      <Footer/>
+    </div>
+  );
+}
+
+function Heading() {
+  return (
+    <h1 className="Heading">Noah Sausen</h1>
+  );
+}
+
+function Details(){
+  return (
+    <div className="Details">
+      <p>React</p>
+      <p>JavaScript</p>
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>Java</p>
+      <p>Python</p>
+    </div>
+  );
+}
+
+function LinkContainer() {
+  return (
+    <div className="LinkContainer">
+      <Link icon={githubIcon} url="https://github.com/noahsausen" name="Github"/>
+      <Link icon={emailIcon} url="mailto:noahsausen@gmail.com" name="Email" extraClass=" emailPadding"/>
+      <Link icon={instagramIcon} url="https://www.instagram.com/noahsausen" name="Instagram"/>
+    </div>
+  );
+}
+
+function Link({icon, url, name, extraClass=""}) {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer"><img className={"LinkIcon" + extraClass} src={icon} alt={name + " logo"}/></a>
+  );
+}
+
+function Footer() {
+  return (
+    <p className="Footer">© {new Date().getFullYear()} Noah Sausen</p>
+  );
+}
