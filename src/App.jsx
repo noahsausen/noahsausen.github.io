8306f1452480417c5249816b259e984e4bd506c0
noahsausen <noahsausen@gmail.com>
2026-07-12 21:18:41 -0400

    copy custom setup, assets, jsx, and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import githubIcon from "./assets/github.svg";
+import emailIcon from "./assets/email.svg";
+import instagramIcon from "./assets/instagram.svg";
+import discordIcon from "./assets/discord.svg";
 
+import remarksThumb from "./assets/remarks.png";
+
+export default function App() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <main className="app">
+        <Prompt/>
+        <Heading/>
+        <Bio/>
+        <WhatIDo/>
+        <Languages/>
+        <Connect/>
+        <Footer/>
+      </main>
+  );
+}
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+function Prompt() {
+  return (
+      <p className="terminal">hello@web ~ % whoami</p>
   )
 }
 
-export default App
+function Heading() {
+  return (
+      <h1 className="heading">Noah Sausen</h1>
+  )
+}
+
+function Bio() {
+  return (
+      <p className="terminal">I'm a high school student in Rye Brook, NY aspiring to be a software engineer.
+        I'm also interested in live sound and <a href="https://flipper.net" target="_blank" rel="noopener noreferrer">pentesting</a>.</p>
+  );
+}
+
+function WhatIDo() {
+  return (
+      <div className="terminal">
+        <p>hello@web ~ % ls <b>experience</b></p>
+        <p>SummerTech/</p>
+        <p>├── Coding Teacher</p>
+        <p>└── <mark>CIT Summer 2026</mark></p>
+        {/*<p><b>What I Do</b></p>*/}
+        {/*<ul>*/}
+        {/*    <li><mark>Currently: Coding Teacher @ <a href="https://www.summertech.net" target="_blank" rel="noopener noreferrer">SummerTech</a></mark></li>*/}
+        {/*    <li>Softwar4e Officer @ <a href="https://jellyfishbbhs.com" target="_blank" rel="noopener noreferrer">FTC Team 4654 Jellyfish</a></li>*/}
+        {/*    <li>Sound Designer @ <a href="https://www.instagram.com/bbhstheater" target="_blank" rel="noopener noreferrer">BBHS Theatre</a></li>*/}
+        {/*</ul>*/}
+      </div>
+  )
+}
+
+function Languages() {
+  return (
+      <div className="terminal">
+        <p>hello@web ~ % ls <b>skills</b></p>
+        <ul>
+          <li>Java + FTC RobotCore</li>
+          <li>Python</li>
+          <li>HTML/CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+      </div>
+  )
+}
+
+function Connect() {
+  return (
+      <div className="ModularBlock Connect">
+        <h3>Connect</h3>
+        <div className="LinkContainer">
+          <div className="LinkContainer">
+            <Link iconFile={githubIcon} url="https://github.com/noahsausen" name="Github"/>
+            <Link iconFile={emailIcon} url="mailto:noahsausen@gmail.com" name="Email" extraClass=" emailPadding"/>
+          </div>
+          <div className="LinkContainer">
+            <Link iconFile={instagramIcon} url="https://www.instagram.com/noahsausen" name="Instagram"/>
+            <Link iconFile={discordIcon} url="https://discord.com/users/990740976043704370" name="Discord"/>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+function Link({iconFile, url, name, extraClass=""}) {
+  return (
+      <a href={url} target="_blank" rel="noopener noreferrer"><img className={"LinkIcon" + extraClass} src={iconFile} alt={name + " logo"}/></a>
+  );
+}
+
+function Projects() {
+  return (
+      <div className="ModularBlock Projects">
+        <h3>Projects</h3>
+        <Project name="Remarks" thumbnail={remarksThumb} urlApp="https://remarks-app.vercel.app" urlCode="https://github.com/noahsausen/Remarks"/>
+      </div>
+  );
+}
+
+function Project({name, thumbnail, urlApp, urlCode}) {
+  const [visible, setVisible] = useState(true);
+
+  return (
+      <div className="Project" style={{backgroundImage: `url(${thumbnail})`,}} onClick={() => setVisible(!visible)} onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
+        <div className={`ProjectOptions ${visible ? "visible" : ""}`} id={name + "Options"}>
+          <p>{name}</p>
+          <div className="ProjectOptionsButtons">
+            <a href={urlApp} target="_blank" rel="noopener noreferrer">App</a>
+            <a href={urlCode} target="_blank" rel="noopener noreferrer">Code</a>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+function Footer() {
+  return (
+      <p className="Footer">&copy; {new Date().getFullYear()} Noah Sausen &bull; Built with React</p>
+  );
+}
