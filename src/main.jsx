578cc4a8d31c9d3fd140e4f00a113910067f8796
noahsausen <noahsausen@gmail.com>
2026-07-13 09:01:35 -0400

    remove index.css and some cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+// import './index.css' https://github.com/vitejs/vite/blob/main/packages/create-vite/template-react/src/index.css
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
